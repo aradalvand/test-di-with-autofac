4de3ef94a425d788619080b34a1f1ac38a5d9627
Arad Alvand <aradalvand@gmail.com>
2025-04-27 17:34:36 -0400

    doesn't work — fuck microsoft engineers

--- Program.cs
+++ Program.cs
@@ -3,8 +3,8 @@ using Microsoft.Extensions.DependencyInjection;
 
 var services = new ServiceCollection();
 services.AddSingleton(typeof(IBar<>), typeof(Bar<>));
-services.AddSingleton<IFoo, Bar<int>>();
-services.AddSingleton<IFoo, Bar<string>>();
+services.AddSingleton<IFoo, IBar<int>>();
+services.AddSingleton<IFoo, IBar<string>>();
 var sp = services.BuildServiceProvider();
 var foos = sp.GetServices<IFoo>().ToList();
 Console.WriteLine($"Built: {foos.Count}");
