6b7fdff6b72de996b1ac6d7f284d95dca2f4cd0c
Arad Alvand <aradalvand@gmail.com>
2025-04-27 17:32:54 -0400

    this obviously works

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+services.AddSingleton(typeof(IBar<>), typeof(Bar<>));
+services.AddSingleton<IFoo, Bar<int>>();
+services.AddSingleton<IFoo, Bar<string>>();
+var sp = services.BuildServiceProvider();
+var foos = sp.GetServices<IFoo>().ToList();
+Console.WriteLine($"Built: {foos.Count}");
+
+public interface IFoo
+{
+
+}
+public interface IBar<T> : IFoo
+{
+
+}
+public class Bar<T> : IBar<T>
+{
+
+}
+
+public class Test(
+    IEnumerable<IFoo> foos
+)
+{
+    public void Print() => Console.WriteLine(string.Join(", ", foos));
+}
