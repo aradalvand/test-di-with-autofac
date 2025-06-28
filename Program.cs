283a28535106e6be15385184db3e12914d20decc
Arad Alvand <aradalvand@gmail.com>
2025-06-28 13:55:16 -0400

    niceeeee

--- Program.cs
+++ Program.cs
@@ -1,30 +1,20 @@
-﻿using System.ComponentModel;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 var services = new ServiceCollection();
-services.AddSingleton(typeof(IBar<>), typeof(Bar<>));
-services.AddSingleton<IFoo>(sp => sp.GetRequiredService<IBar<int>>());
-services.AddSingleton<IFoo>(sp => sp.GetRequiredService<IBar<string>>());
+services.AddSingleton(typeof(IBar<>), typeof(FooBar<>));
+services.AddSingleton(typeof(IBar<>), typeof(GeneralBar<>));
+
 var sp = services.BuildServiceProvider();
-var foos = sp.GetServices<IFoo>().ToList();
-Console.WriteLine($"Built: {foos.Count}");
 
-public interface IFoo
-{
+var bar1 = sp.GetServices<IBar<Foo>>();
+Console.WriteLine($"IBar<Foo>s: {string.Join(", ", bar1.Select(e => e.GetType().Name))}");
 
-}
-public interface IBar<T> : IFoo
-{
+var bar2 = sp.GetServices<IBar<int>>();
+Console.WriteLine($"IBar<int>s: {string.Join(", ", bar2.Select(e => e.GetType().Name))}");
 
-}
-public class Bar<T> : IBar<T>
-{
+public interface IBar<T> : IFoo;
+public class FooBar<T> : IBar<T> where T : IFoo;
+public class GeneralBar<T> : IBar<T>;
 
-}
-
-public class Test(
-    IEnumerable<IFoo> foos
-)
-{
-    public void Print() => Console.WriteLine(string.Join(", ", foos));
-}
+public interface IFoo;
+public class Foo : IFoo;
