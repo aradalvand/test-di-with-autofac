4567a9723c7803df1139b395eb78351631c275f6
Arad Alvand <aradalvand@gmail.com>
2025-08-28 14:14:53 -0400

    not working

--- Program.cs
+++ Program.cs
@@ -1,20 +1,31 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 
 var services = new ServiceCollection();
-services.AddSingleton<IFoo, Foo1>();
-services.AddScoped<IFoo, Foo2>();
+services.AddScoped<IFoo, Foo1>();
+// services.AddSingleton<IServiceScopeFactory, S>();
 
 var sp = services.BuildServiceProvider();
-var singletonFoo = sp.GetRequiredService<IFoo>();
-
-Console.WriteLine($"SINGLETON: {sp.GetRequiredService<IFoo>().GetType()}");
 
 using (var scope = sp.CreateScope())
 {
     var scopedFoo = scope.ServiceProvider.GetRequiredService<IFoo>();
-    Console.WriteLine($"SCOPED: {scope.ServiceProvider.GetRequiredService<IFoo>().GetType()}");
+    Console.WriteLine($"SCOPED: {scopedFoo.GetHashCode()}");
+
+    using (var scope2 = scope.ServiceProvider.CreateScope())
+    {
+        var scopedFoo2 = scope2.ServiceProvider.GetRequiredService<IFoo>();
+        Console.WriteLine($"SCOPED: {scopedFoo2.GetHashCode()}");
+    }
 }
 
 public interface IFoo;
 public class Foo1 : IFoo;
-public class Foo2 : IFoo;
+
+public class S : IServiceScopeFactory
+{
+    public IServiceScope CreateScope()
+    {
+        Console.WriteLine("HERE");
+        throw new NotImplementedException();
+    }
+}
