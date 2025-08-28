2a2f33f0f8e003c6d8178666dea7d3bb6afea754
Arad Alvand <aradalvand@gmail.com>
2025-08-28 15:51:31 -0400

    yep so perfect

--- Program.cs
+++ Program.cs
@@ -48,11 +48,10 @@ public class Foo1 : IFoo;
 public interface IBar;
 public class Bar1 : IBar;
 
-public sealed class CustomServiceProvider : IServiceProvider
+public sealed class CustomServiceProvider : IServiceProvider, IServiceScopeFactory
 {
     private readonly List<Type> _singletonMadeScopedServiceTypes = [];
     private readonly ServiceProvider _underlyingProvider;
-    private readonly ScopeFactory _scopeFactory;
     public CustomServiceProvider(IServiceCollection services)
     {
         foreach (var singletonService in services.Where(s => s.Lifetime is ServiceLifetime.Singleton).ToList())
@@ -64,44 +63,37 @@ public sealed class CustomServiceProvider : IServiceProvider
             _singletonMadeScopedServiceTypes.Add(singletonService.ServiceType);
         }
         _underlyingProvider = services.BuildServiceProvider();
-        _scopeFactory = new(this);
     }
 
-    public object? GetService(Type serviceType)
-    {
-        if (serviceType == typeof(IServiceScopeFactory))
-            return _scopeFactory;
+    object? IServiceProvider.GetService(Type serviceType) =>
+        serviceType == typeof(IServiceScopeFactory)
+            ? this
+            : _underlyingProvider.GetService(serviceType);
 
-        return _underlyingProvider.GetService(serviceType);
-    }
-
-    private sealed class ScopeFactory(
-        CustomServiceProvider parent
-    ) : IServiceScopeFactory
-    {
-        public IServiceScope CreateScope() =>
-            new Scope(parent, this);
-    }
+    IServiceScope IServiceScopeFactory.CreateScope() =>
+        new Scope(this, new(_underlyingProvider.CreateScope));
 
     private sealed class Scope(
         CustomServiceProvider provider,
-        ScopeFactory scopeFactory
+        Lazy<IServiceScope> singletonScope
     ) : IServiceScopeFactory, IServiceScope, IServiceProvider
     {
-        private readonly IServiceScope _scope = provider._underlyingProvider.CreateScope();
-        public IServiceProvider ServiceProvider => this;
+        private readonly Lazy<IServiceScope> _newScope = new(provider._underlyingProvider.CreateScope);
 
-        public IServiceScope CreateScope() => this;
-
-        public void Dispose() { }
-
-        public object? GetService(Type serviceType)
+        IServiceProvider IServiceScope.ServiceProvider => this;
+        IServiceScope IServiceScopeFactory.CreateScope() => new Scope(provider, singletonScope);
+        object? IServiceProvider.GetService(Type serviceType)
         {
             if (serviceType == typeof(IServiceScopeFactory))
                 return this;
 
-            return _scope.ServiceProvider.GetService(serviceType);
+            if (provider._singletonMadeScopedServiceTypes.Contains(serviceType))
+                return singletonScope.Value.ServiceProvider.GetService(serviceType);
+
+            return _newScope.Value.ServiceProvider.GetService(serviceType);
         }
+
+        public void Dispose() { }
     }
 }
 
