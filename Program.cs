277a1a85d532282634f00a61f07ce126d747d835
Arad Alvand <aradalvand@gmail.com>
2026-03-22 12:25:46 -0400

    make it presentable

--- Program.cs
+++ Program.cs
@@ -1,31 +1,28 @@
-﻿using Autofac;
-using Autofac.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 var services = new ServiceCollection();
-// services.AddScoped(typeof(IFuck<>), typeof(FuckForEverything<>));
-services.AddScoped(typeof(IFuck<>), typeof(FuckForOmissibles<>));
+// services.AddScoped(typeof(IFoo<>), typeof(FooForEverything<>));
+services.AddScoped(typeof(IFoo<>), typeof(FooForSpecials<>));
 
 var sp = services.BuildServiceProvider();
 
 var isService = sp.GetRequiredService<IServiceProviderIsService>();
-Console.WriteLine($"IFuck<Omissible> has services? {isService.IsService(typeof(IFuck<Omissible>))}");
-Console.WriteLine($"IFuck<string> has services? {isService.IsService(typeof(IFuck<string>))}");
+Console.WriteLine($"IFoo<Special> has services? {isService.IsService(typeof(IFoo<Special>))}");
+Console.WriteLine($"IFoo<object> has services? {isService.IsService(typeof(IFoo<object>))}");
 
 using var scope = sp.CreateScope();
 
-var one = scope.ServiceProvider.GetServices<IFuck<Omissible>>();
-Console.WriteLine($"For IFuck<Omissible>: ({one.Count()}) {string.Join(", ", one)}");
+var one = scope.ServiceProvider.GetServices<IFoo<Special>>();
+Console.WriteLine($"For IFoo<Special>: ({one.Count()}) {string.Join(", ", one)}");
 
-var two = scope.ServiceProvider.GetServices<IFuck<string>>();
-Console.WriteLine($"For IFuck<string>: ({two.Count()}) {string.Join(", ", two)}");
+var two = scope.ServiceProvider.GetServices<IFoo<object>>();
+Console.WriteLine($"For IFoo<object>: ({two.Count()}) {string.Join(", ", two)}");
 
-public interface IFuck<T>;
+public interface IFoo<T>;
 
-public sealed class FuckForEverything<T> : IFuck<T>;
-public sealed class FuckForOmissibles<T> : IFuck<T>
-    where T : IOmissible;
+public sealed class FooForEverything<T> : IFoo<T>;
+public sealed class FooForSpecials<T> : IFoo<T>
+    where T : ISpecial;
 
-public interface IOmissible;
-public sealed class Omissible : IOmissible;
+public interface ISpecial;
+public sealed class Special : ISpecial;
