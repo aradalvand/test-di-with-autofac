441c2a73537f15bcc2cfbb06b0dc82291df743a4
Arad Alvand <aradalvand@gmail.com>
2026-03-22 11:52:32 -0400

    ffs why doesn't it work in avesta then

--- Program.cs
+++ Program.cs
@@ -5,27 +5,21 @@ using Microsoft.Extensions.Hosting;
 
 var services = new ServiceCollection();
 services.AddSingleton(typeof(IFuck<>), typeof(FuckForEverything<>));
-services.AddSingleton(typeof(IFuck<>), typeof(FuckForValueTypes<>));
+services.AddSingleton(typeof(IFuck<>), typeof(FuckForOmissibles<>));
 
 var sp = services.BuildServiceProvider();
 
-var one = sp.GetServices<IFuck<string>>();
+var one = sp.GetServices<IFuck<Omissible>>();
 Console.WriteLine($"{string.Join(", ", one)}");
 
-var two = sp.GetServices<IFuck<int>>();
+var two = sp.GetServices<IFuck<string>>();
 Console.WriteLine($"{string.Join(", ", two)}");
 
-public interface IFuck<T>
-{
+public interface IFuck<T>;
 
-}
+public sealed class FuckForEverything<T> : IFuck<T>;
+public sealed class FuckForOmissibles<T> : IFuck<T>
+    where T : IOmissible;
 
-public sealed class FuckForEverything<T> : IFuck<T>
-{
-
-}
-public sealed class FuckForValueTypes<T> : IFuck<T>
-    where T : struct
-{
-
-}
+public interface IOmissible;
+public sealed class Omissible : IOmissible;
